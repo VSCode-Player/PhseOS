7bea04c9600e6349400b6e880e836def7aa16df4
锟斤拷Unicode <14876294+vscode-player@user.noreply.gitee.com>
2026-01-13 23:38:55 +0800

    Update MSG command(have bugs)

--- hardware/DISK/system/phsex/library/stdio/output_op.py
+++ hardware/DISK/system/phsex/library/stdio/output_op.py
@@ -1,10 +1,16 @@
 from PhseXlib.locals import * # type: ignore
 
 Encode_dict = GetEncoding("PhseEncode")
+format_list = {"n":"\n"}
 
 def msg(*string):
     for i in string:
         if i[0] == "b":
             print(Encode_dict[i[1:]])
         else:
-            print(i[1:-1])
+            for j,k in zip(i[1:-1],range(len(i))):
+                if j == "\\":
+                    print(format_list[i[k+1]],end="")
+                else:
+                    print(j, end="")
+                # print(i[1:-1])
